cc9b7735d7d52d859f46e5bcb6f07ae9074f8518
Yauheni Vadzianovich <yauheni.vadzianovich@innowise-group.com>
2022-04-14 13:58:41 +0300

    Project setup

---
+++ WebWrapper6/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  WebWrapper6
+//
+//  Created by Yauheni Vadzianovich on 14.04.22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+}
